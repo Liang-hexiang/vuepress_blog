b32987870de78d099d62557ec663789ca20f7e72
lianghexiang <lhx110396@163.com>
2023-07-14 12:46:56 +0800

    修改根文件路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,10 +2,9 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
-
+  base: "/space",
   lang: "zh-CN",
-  title: "百合网",
+  title: "",
   description: "集搞笑、技术、娱乐、八卦、新闻为一体的全方位未来式网站！",
 
   theme,
