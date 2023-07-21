a1b32fa2dc6c4a1625cf1dc51353ba51c1819e1d
lianghexiang <lhx110396@163.com>
2023-07-21 13:35:02 +0800

    增加DBUtils日记

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,12 +2,21 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/space",
+  base: "/space/",
   lang: "zh-CN",
   title: "",
   description: "欢迎来到我的博客空间",
 
   theme,
+  head: [
+    [
+      "meta",
+      {
+        name:"referrer",
+        content:"no-referrer"
+      }
+    ],
+  ]
 
   // Enable it with pwa
   // shouldPrefetch: false,
