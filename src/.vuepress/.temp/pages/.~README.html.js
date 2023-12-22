70bc6a84ba77241addfadd99743354fcf3c7d87f
lianghexiang <lhx110396@163.com>
2023-12-22 09:26:28 +0800

    修改个人介绍页内容

---
+++ src/.vuepress/.temp/pages/.~README.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-21564982\",\"path\":\"/.~README.html\",\"title\":\"博客主页\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"layout\":\"BlogHome\",\"icon\":\"home\",\"title\":\"博客主页\",\"bgImage\":\"https://img.tucang.cc/api/image/show/46d84dfc18dde27647210a3aa2319b0a\",\"bgImageDark\":\"https://img.tucang.cc/api/image/show/56b485174393d21065eee635c2fbef79\",\"heroText\":\"懒羊羊\",\"heroFullScreen\":true,\"tagline\":\"欢迎来到我的博客空间\",\"projects\":[{\"icon\":\"project\",\"name\":\"项目名称\",\"desc\":\"项目详细描述\",\"link\":\"https://你的项目链接\"},{\"icon\":\"friend\",\"name\":\"伙伴名称\",\"desc\":\"伙伴详细介绍\",\"link\":\"https://你的伙伴链接\"}],\"features\":[{\"icon\":\"⚡️\",\"title\":\"vite + vue3\",\"details\":\"两岸猿声啼不住，轻舟已过万重山\"},{\"icon\":\"🖖\",\"title\":\"知新\",\"details\":\"好记性不如烂笔头\"},{\"icon\":\"🛠️\",\"title\":\"温故\",\"details\":\"书读百遍其义自见\"}],\"footer\":\"学无止境\",\"description\":\"\"},\"headers\":[],\"readingTime\":{\"minutes\":0.44,\"words\":132},\"filePathRelative\":\".~README.md\",\"excerpt\":\"\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
