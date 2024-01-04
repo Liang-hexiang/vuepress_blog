a9eef251587a23114253dcd1a465c2d5653a14ef
lianghexiang <51989260+lianghexiang@users.noreply.github.com>
2024-01-04 09:30:28 +0000

    Deploying to gh-pages from @ lianghexiang/space@9badab5358ea858e7b890c68cb8c94ddd968e74c 🚀

--- assets/工厂模式2.html-1119d72b.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as o,e,f as r}from"./app-267153ed.js";const _={},c=e("h1",{id:"工厂模式",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#工厂模式","aria-hidden":"true"},"#"),r(" 工厂模式")],-1),s=[c];function n(d,i){return a(),o("div",null,s)}const l=t(_,[["render",n],["__file","工厂模式2.html.vue"]]);export{l as default};
