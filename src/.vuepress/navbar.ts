5d309f78d9878fd6d5d2897b217ec4f2ca94e134
lianghexiang <lhx110396@163.com>
2024-01-06 17:56:52 +0800

    修改文章目录

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -15,6 +15,7 @@ export default navbar([
                     {text: 'Polygon面积计算', icon: 'book', link: 'Polygon面积计算'},
                     {text: 'Python2转Python3', icon: 'book', link: 'Python2转Python3'},
                     {text: 'Python添加环境变量', icon: 'book', link: 'Python添加环境变量'},
+                    {text: "DBUtils小问题", icon: "pen", link: "Linux环境下DBUtils导入的问题"},
 
                 ],
             },
@@ -52,19 +53,19 @@ export default navbar([
         prefix: "/anything/",
         children: [
             {
-                text: "日记",
-                icon: "pen",
-                prefix: "note/",
+                text: "小记",
+                icon: "book",
+                prefix: "笔记/",
                 children: [
-                    {text: "DBUtils小问题", icon: "pen", link: "Linux环境下DBUtils导入的问题"},
                     {text: '下雪啦', icon: 'snow', link: "下雪啦"}
                 ],
             },
             {
-                text: "笔记",
+                text: "读书笔记",
                 icon: "book",
-                prefix: "笔记/",
+                prefix: "book_note/",
                 children: [
+
                 ],
             },
         ],
