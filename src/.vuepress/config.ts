91c9a8407ed805d101c904ce078e07762bf6ce31
lianghexiang <lhx110396@163.com>
2024-01-25 16:49:44 +0800

    修改vue图标

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,4 @@
+import { getDirname, path } from "@vuepress/utils";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -6,7 +7,12 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "",
   description: "欢迎来到酥肉丸子的博客空间",
-
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+        __dirname,
+        "./components/BlogHero.vue",
+    ),
+  },
   theme,
   head: [
 
