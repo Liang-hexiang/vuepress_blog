b5bc7415b1a5c3567e6a28c55428aceb6dddec85
lianghexiang <lhx110396@163.com>
2024-01-26 08:47:58 +0800

    修改vue图标

--- src/.vuepress/components/BlogHero.vue
+++ src/.vuepress/components/BlogHero.vue
@@ -1,9 +1,7 @@
 <!-- .vuepress/components/BlogHero.vue -->
 <script setup lang="ts">
 import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
-import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
 import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
-
 </script>
 
 <template>
