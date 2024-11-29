3124c04272116e3938c47bd1b6d13147f5cfbb73
lianghexiang <lhx110396@163.com>
2024-11-29 16:06:31 +0800

    修改侧边栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,29 +1,18 @@
 import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-    "/": [
-        {
-            text: "博客",
-            prefix: 'posts/',
-            collapsible: true,
-            children: [
-                {text: 'gRPC', icon: 'fas fa-book', prefix: 'gRPC/', collapsible: true, children: ['gRPC服务']},
-                {
-                    text: 'Python',
-                    icon: 'fab fa-python',
-                    prefix: 'python/',
-                    collapsible: true,
-                    children: ['DBUtils导入', 'polygon面积计算', 'Python2转Python3']
-                },
-            ]
-        },
-        {
-            text: "设计模式",
-            prefix: 'design_pattern/',
-            collapsible: true,
-            children: [
-                '单例模式', '工厂模式', '门面模式'
-            ]
-        },
-    ]
+    '/posts/': 'structure',
+    '/books/': 'structure',
+    '/人工智能/': 'structure',
+    '/diary/': 'structure'
 });
+
+//
+// {
+//     text: '博客',
+//         icon: 'fas fa-book-open',
+//     prefix:  "/posts/",
+//     children: [
+//
+// ]
+// }
