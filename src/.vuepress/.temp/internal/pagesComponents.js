6cb804efd7b17bf8fbae36ac01973e9362e50933
lianghexiang <lhx110396@163.com>
2025-02-11 08:29:21 +0800

    修改分支

--- src/.vuepress/.temp/internal/pagesComponents.js
+++ src/.vuepress/.temp/internal/pagesComponents.js
@@ -7,12 +7,12 @@ export const pagesComponents = {
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"E:/code/space/src/.vuepress/.temp/pages/index.html.vue")),
   // path: /slides.html
   "v-2e3eac9e": defineAsyncComponent(() => import(/* webpackChunkName: "v-2e3eac9e" */"E:/code/space/src/.vuepress/.temp/pages/slides.html.vue")),
+  // path: /books/%E4%BA%BA%E4%B9%8B%E8%A7%89%E9%86%92/%E4%B8%80%E5%88%87%E9%97%AE%E9%A2%98%E7%9A%84%E8%B5%B7%E6%BA%90.html
+  "v-17e61d0d": defineAsyncComponent(() => import(/* webpackChunkName: "v-17e61d0d" */"E:/code/space/src/.vuepress/.temp/pages/books/人之觉醒/一切问题的起源.html.vue")),
   // path: /diary/%E5%B0%8F%E8%AE%B0/%E4%B8%8B%E9%9B%AA%E5%95%A6.html
   "v-f51f6220": defineAsyncComponent(() => import(/* webpackChunkName: "v-f51f6220" */"E:/code/space/src/.vuepress/.temp/pages/diary/小记/下雪啦.html.vue")),
   // path: /diary/%E5%B0%8F%E8%AE%B0/%E5%A5%BD%E7%94%A8%E7%BD%91%E7%AB%99%E6%94%B6%E8%97%8F.html
   "v-542f314a": defineAsyncComponent(() => import(/* webpackChunkName: "v-542f314a" */"E:/code/space/src/.vuepress/.temp/pages/diary/小记/好用网站收藏.html.vue")),
-  // path: /books/%E4%BA%BA%E4%B9%8B%E8%A7%89%E9%86%92/%E4%B8%80%E5%88%87%E9%97%AE%E9%A2%98%E7%9A%84%E8%B5%B7%E6%BA%90.html
-  "v-17e61d0d": defineAsyncComponent(() => import(/* webpackChunkName: "v-17e61d0d" */"E:/code/space/src/.vuepress/.temp/pages/books/人之觉醒/一切问题的起源.html.vue")),
   // path: /posts/design_pattern/%E5%8D%95%E4%BE%8B%E6%A8%A1%E5%BC%8F.html
   "v-4f323a05": defineAsyncComponent(() => import(/* webpackChunkName: "v-4f323a05" */"E:/code/space/src/.vuepress/.temp/pages/posts/design_pattern/单例模式.html.vue")),
   // path: /posts/design_pattern/%E5%B7%A5%E5%8E%82%E6%A8%A1%E5%BC%8F.html
@@ -31,26 +31,34 @@ export const pagesComponents = {
   "v-26844e37": defineAsyncComponent(() => import(/* webpackChunkName: "v-26844e37" */"E:/code/space/src/.vuepress/.temp/pages/posts/pyspark/PySpark初学者教程.html.vue")),
   // path: /posts/pyspark/SparkSession%E6%95%99%E7%A8%8B.html
   "v-30e593e0": defineAsyncComponent(() => import(/* webpackChunkName: "v-30e593e0" */"E:/code/space/src/.vuepress/.temp/pages/posts/pyspark/SparkSession教程.html.vue")),
+  // path: /posts/pyspark/spark%E5%BF%AB%E9%80%9F%E5%A4%A7%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90%E4%B9%8BSpark%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90%E5%AF%BC%E8%AE%BA.html
+  "v-262ffd2c": defineAsyncComponent(() => import(/* webpackChunkName: "v-262ffd2c" */"E:/code/space/src/.vuepress/.temp/pages/posts/pyspark/spark快速大数据分析之Spark数据分析导论.html.vue")),
   // path: /posts/python/DBUtils%E5%AF%BC%E5%85%A5.html
   "v-13a02a29": defineAsyncComponent(() => import(/* webpackChunkName: "v-13a02a29" */"E:/code/space/src/.vuepress/.temp/pages/posts/python/DBUtils导入.html.vue")),
   // path: /posts/tools/PicgoGitHub%E6%90%AD%E5%BB%BA%E5%9B%BE%E5%BA%8A.html
   "v-34cef64b": defineAsyncComponent(() => import(/* webpackChunkName: "v-34cef64b" */"E:/code/space/src/.vuepress/.temp/pages/posts/tools/PicgoGitHub搭建图床.html.vue")),
-  // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/%E5%90%91%E9%87%8F%E6%95%B0%E6%8D%AE%E5%BA%93.html
-  "v-2adb056f": defineAsyncComponent(() => import(/* webpackChunkName: "v-2adb056f" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/向量数据库.html.vue")),
   // path: /posts/Vue/Vue%E5%9F%BA%E7%A1%80.html
   "v-66d370d9": defineAsyncComponent(() => import(/* webpackChunkName: "v-66d370d9" */"E:/code/space/src/.vuepress/.temp/pages/posts/Vue/Vue基础.html.vue")),
   // path: /posts/Vue/vue%E5%AE%A2%E6%88%B7%E7%AB%AF.html
   "v-3ae980e2": defineAsyncComponent(() => import(/* webpackChunkName: "v-3ae980e2" */"E:/code/space/src/.vuepress/.temp/pages/posts/Vue/vue客户端.html.vue")),
+  // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/llm-prompt%E6%8F%90%E7%A4%BA%E5%8E%9F%E5%88%99.html
+  "v-60c98e23": defineAsyncComponent(() => import(/* webpackChunkName: "v-60c98e23" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/llm-prompt提示原则.html.vue")),
+  // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/%E5%90%91%E9%87%8F%E6%95%B0%E6%8D%AE%E5%BA%93.html
+  "v-2adb056f": defineAsyncComponent(() => import(/* webpackChunkName: "v-2adb056f" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/向量数据库.html.vue")),
+  // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/%E5%A4%A7%E8%AF%AD%E8%A8%80%E6%A8%A1%E5%9E%8B%E7%AE%80%E4%BB%8B.html
+  "v-05a88cd5": defineAsyncComponent(() => import(/* webpackChunkName: "v-05a88cd5" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/大语言模型简介.html.vue")),
+  // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/%E6%A3%80%E7%B4%A2%E5%A2%9E%E5%BC%BA%E7%94%9F%E6%88%90RAG.html
+  "v-706654d0": defineAsyncComponent(() => import(/* webpackChunkName: "v-706654d0" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/检索增强生成RAG.html.vue")),
   // path: /404.html
   "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"E:/code/space/src/.vuepress/.temp/pages/404.html.vue")),
-  // path: /diary/%E5%B0%8F%E8%AE%B0/
-  "v-40f67ddd": defineAsyncComponent(() => import(/* webpackChunkName: "v-40f67ddd" */"E:/code/space/src/.vuepress/.temp/pages/diary/小记/index.html.vue")),
-  // path: /diary/
-  "v-7a372905": defineAsyncComponent(() => import(/* webpackChunkName: "v-7a372905" */"E:/code/space/src/.vuepress/.temp/pages/diary/index.html.vue")),
   // path: /books/%E4%BA%BA%E4%B9%8B%E8%A7%89%E9%86%92/
   "v-5059160d": defineAsyncComponent(() => import(/* webpackChunkName: "v-5059160d" */"E:/code/space/src/.vuepress/.temp/pages/books/人之觉醒/index.html.vue")),
   // path: /books/
   "v-772846fe": defineAsyncComponent(() => import(/* webpackChunkName: "v-772846fe" */"E:/code/space/src/.vuepress/.temp/pages/books/index.html.vue")),
+  // path: /diary/%E5%B0%8F%E8%AE%B0/
+  "v-40f67ddd": defineAsyncComponent(() => import(/* webpackChunkName: "v-40f67ddd" */"E:/code/space/src/.vuepress/.temp/pages/diary/小记/index.html.vue")),
+  // path: /diary/
+  "v-7a372905": defineAsyncComponent(() => import(/* webpackChunkName: "v-7a372905" */"E:/code/space/src/.vuepress/.temp/pages/diary/index.html.vue")),
   // path: /posts/design_pattern/
   "v-f0dfdf2a": defineAsyncComponent(() => import(/* webpackChunkName: "v-f0dfdf2a" */"E:/code/space/src/.vuepress/.temp/pages/posts/design_pattern/index.html.vue")),
   // path: /posts/
@@ -67,12 +75,12 @@ export const pagesComponents = {
   "v-3ea18a3e": defineAsyncComponent(() => import(/* webpackChunkName: "v-3ea18a3e" */"E:/code/space/src/.vuepress/.temp/pages/posts/python/index.html.vue")),
   // path: /posts/tools/
   "v-2951b8e9": defineAsyncComponent(() => import(/* webpackChunkName: "v-2951b8e9" */"E:/code/space/src/.vuepress/.temp/pages/posts/tools/index.html.vue")),
+  // path: /posts/Vue/
+  "v-635a6bfe": defineAsyncComponent(() => import(/* webpackChunkName: "v-635a6bfe" */"E:/code/space/src/.vuepress/.temp/pages/posts/Vue/index.html.vue")),
   // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/LLM/
   "v-34d3e8ce": defineAsyncComponent(() => import(/* webpackChunkName: "v-34d3e8ce" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/LLM/index.html.vue")),
   // path: /%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/
   "v-1d6afbcc": defineAsyncComponent(() => import(/* webpackChunkName: "v-1d6afbcc" */"E:/code/space/src/.vuepress/.temp/pages/人工智能/index.html.vue")),
-  // path: /posts/Vue/
-  "v-635a6bfe": defineAsyncComponent(() => import(/* webpackChunkName: "v-635a6bfe" */"E:/code/space/src/.vuepress/.temp/pages/posts/Vue/index.html.vue")),
   // path: /category/
   "v-5bc93818": defineAsyncComponent(() => import(/* webpackChunkName: "v-5bc93818" */"E:/code/space/src/.vuepress/.temp/pages/category/index.html.vue")),
   // path: /tag/
@@ -83,18 +91,18 @@ export const pagesComponents = {
   "v-154dc4c4": defineAsyncComponent(() => import(/* webpackChunkName: "v-154dc4c4" */"E:/code/space/src/.vuepress/.temp/pages/star/index.html.vue")),
   // path: /timeline/
   "v-01560935": defineAsyncComponent(() => import(/* webpackChunkName: "v-01560935" */"E:/code/space/src/.vuepress/.temp/pages/timeline/index.html.vue")),
-  // path: /category/%E7%94%BB%E5%86%8C/
-  "v-605620d1": defineAsyncComponent(() => import(/* webpackChunkName: "v-605620d1" */"E:/code/space/src/.vuepress/.temp/pages/category/画册/index.html.vue")),
-  // path: /tag/%E7%BD%91%E7%AB%99/
-  "v-efadec50": defineAsyncComponent(() => import(/* webpackChunkName: "v-efadec50" */"E:/code/space/src/.vuepress/.temp/pages/tag/网站/index.html.vue")),
-  // path: /category/%E7%BD%91%E5%9D%80%E6%94%B6%E8%97%8F/
-  "v-6efe3f6e": defineAsyncComponent(() => import(/* webpackChunkName: "v-6efe3f6e" */"E:/code/space/src/.vuepress/.temp/pages/category/网址收藏/index.html.vue")),
-  // path: /tag/%E7%84%A6%E8%99%91/
-  "v-677a8581": defineAsyncComponent(() => import(/* webpackChunkName: "v-677a8581" */"E:/code/space/src/.vuepress/.temp/pages/tag/焦虑/index.html.vue")),
   // path: /category/%E8%AF%BB%E4%B9%A6%E7%AC%94%E8%AE%B0/
   "v-0a695906": defineAsyncComponent(() => import(/* webpackChunkName: "v-0a695906" */"E:/code/space/src/.vuepress/.temp/pages/category/读书笔记/index.html.vue")),
+  // path: /tag/%E7%84%A6%E8%99%91/
+  "v-677a8581": defineAsyncComponent(() => import(/* webpackChunkName: "v-677a8581" */"E:/code/space/src/.vuepress/.temp/pages/tag/焦虑/index.html.vue")),
+  // path: /category/%E7%94%BB%E5%86%8C/
+  "v-605620d1": defineAsyncComponent(() => import(/* webpackChunkName: "v-605620d1" */"E:/code/space/src/.vuepress/.temp/pages/category/画册/index.html.vue")),
   // path: /tag/%E7%AC%94%E8%AE%B0/
   "v-7329824f": defineAsyncComponent(() => import(/* webpackChunkName: "v-7329824f" */"E:/code/space/src/.vuepress/.temp/pages/tag/笔记/index.html.vue")),
+  // path: /category/%E7%BD%91%E5%9D%80%E6%94%B6%E8%97%8F/
+  "v-6efe3f6e": defineAsyncComponent(() => import(/* webpackChunkName: "v-6efe3f6e" */"E:/code/space/src/.vuepress/.temp/pages/category/网址收藏/index.html.vue")),
+  // path: /tag/%E7%BD%91%E7%AB%99/
+  "v-efadec50": defineAsyncComponent(() => import(/* webpackChunkName: "v-efadec50" */"E:/code/space/src/.vuepress/.temp/pages/tag/网站/index.html.vue")),
   // path: /category/python/
   "v-78cbe7bb": defineAsyncComponent(() => import(/* webpackChunkName: "v-78cbe7bb" */"E:/code/space/src/.vuepress/.temp/pages/category/python/index.html.vue")),
   // path: /tag/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F/
@@ -119,12 +127,12 @@ export const pagesComponents = {
   "v-14145d44": defineAsyncComponent(() => import(/* webpackChunkName: "v-14145d44" */"E:/code/space/src/.vuepress/.temp/pages/category/工具/index.html.vue")),
   // path: /tag/mysql/
   "v-1bee38ca": defineAsyncComponent(() => import(/* webpackChunkName: "v-1bee38ca" */"E:/code/space/src/.vuepress/.temp/pages/tag/mysql/index.html.vue")),
-  // path: /category/%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/
-  "v-72c4ce0d": defineAsyncComponent(() => import(/* webpackChunkName: "v-72c4ce0d" */"E:/code/space/src/.vuepress/.temp/pages/category/人工智能/index.html.vue")),
-  // path: /tag/%E6%A1%86%E6%9E%B6/
-  "v-f4d3cd72": defineAsyncComponent(() => import(/* webpackChunkName: "v-f4d3cd72" */"E:/code/space/src/.vuepress/.temp/pages/tag/框架/index.html.vue")),
   // path: /category/vue/
   "v-65f6d381": defineAsyncComponent(() => import(/* webpackChunkName: "v-65f6d381" */"E:/code/space/src/.vuepress/.temp/pages/category/vue/index.html.vue")),
+  // path: /tag/%E6%A1%86%E6%9E%B6/
+  "v-f4d3cd72": defineAsyncComponent(() => import(/* webpackChunkName: "v-f4d3cd72" */"E:/code/space/src/.vuepress/.temp/pages/tag/框架/index.html.vue")),
+  // path: /category/%E4%BA%BA%E5%B7%A5%E6%99%BA%E8%83%BD/
+  "v-72c4ce0d": defineAsyncComponent(() => import(/* webpackChunkName: "v-72c4ce0d" */"E:/code/space/src/.vuepress/.temp/pages/category/人工智能/index.html.vue")),
   // path: /tag/%E5%A4%A7%E6%95%B0%E6%8D%AE/
   "v-47d2837f": defineAsyncComponent(() => import(/* webpackChunkName: "v-47d2837f" */"E:/code/space/src/.vuepress/.temp/pages/tag/大数据/index.html.vue")),
   // path: /tag/%E6%95%99%E7%A8%8B/
@@ -137,10 +145,14 @@ export const pagesComponents = {
   "v-1382eb6a": defineAsyncComponent(() => import(/* webpackChunkName: "v-1382eb6a" */"E:/code/space/src/.vuepress/.temp/pages/tag/picgo/index.html.vue")),
   // path: /tag/github/
   "v-132a6ac4": defineAsyncComponent(() => import(/* webpackChunkName: "v-132a6ac4" */"E:/code/space/src/.vuepress/.temp/pages/tag/github/index.html.vue")),
-  // path: /tag/%E5%90%91%E9%87%8F%E6%95%B0%E6%8D%AE%E5%BA%93/
-  "v-d7800d76": defineAsyncComponent(() => import(/* webpackChunkName: "v-d7800d76" */"E:/code/space/src/.vuepress/.temp/pages/tag/向量数据库/index.html.vue")),
-  // path: /tag/llm/
-  "v-b30c33a0": defineAsyncComponent(() => import(/* webpackChunkName: "v-b30c33a0" */"E:/code/space/src/.vuepress/.temp/pages/tag/llm/index.html.vue")),
   // path: /tag/%E5%89%8D%E7%AB%AF/
   "v-95f987f4": defineAsyncComponent(() => import(/* webpackChunkName: "v-95f987f4" */"E:/code/space/src/.vuepress/.temp/pages/tag/前端/index.html.vue")),
+  // path: /tag/prompt/
+  "v-3ccb8586": defineAsyncComponent(() => import(/* webpackChunkName: "v-3ccb8586" */"E:/code/space/src/.vuepress/.temp/pages/tag/prompt/index.html.vue")),
+  // path: /tag/llm/
+  "v-b30c33a0": defineAsyncComponent(() => import(/* webpackChunkName: "v-b30c33a0" */"E:/code/space/src/.vuepress/.temp/pages/tag/llm/index.html.vue")),
+  // path: /tag/%E5%90%91%E9%87%8F%E6%95%B0%E6%8D%AE%E5%BA%93/
+  "v-d7800d76": defineAsyncComponent(() => import(/* webpackChunkName: "v-d7800d76" */"E:/code/space/src/.vuepress/.temp/pages/tag/向量数据库/index.html.vue")),
+  // path: /tag/rag/
+  "v-b3071336": defineAsyncComponent(() => import(/* webpackChunkName: "v-b3071336" */"E:/code/space/src/.vuepress/.temp/pages/tag/rag/index.html.vue")),
 }
---
+++ src/posts/pyspark/spark快速大数据分析之Spark数据分析导论.md
@@ -0,0 +1,44 @@
+---
+date: 2024-02-01
+icon: 'map'
+author: '梁贺祥'
+cover: 'https://img.tucang.cc/api/image/show/498496728f8ad787045c66c3af161ab1'
+category:
+    - 技术
+    - IT
+tag:
+    - 大数据
+    - 教程
+    - PySpark
+sticky: 6
+---
+
+
+
+# Spark数据分析导论
+
+::: tip spark快速大数据分析-第一章节
+记录第一章节讲解内容
+:::
+
+
+
+## Spark是什么:question:
+
+1. spark 是一个用来实现快速而通用的集群计算的平台。  
+2. Spark能够在内存中进行计算.
+3. 提供丰富的语言接口，包括：Python、Java、Scala
+4. Spark可以运行在Hadoop集群上。
+
+​	
+
+## Spark软件栈
+
+> Spark 项目包含多个紧密集成的组件  。Spark 的核心是一个对由很多计算任务组成的、运行在多个工作机器或者是一个计算集群上的应用进行调度、 分发以及监控的计算引擎。  
+
+由于Spark 的核心引擎有着速度快和通用的特点  ，Spark 还支持为各种不同应用场景专门设计的高级组件  ，比如
+
+SQL 和机器学习等  。
+
+
+
